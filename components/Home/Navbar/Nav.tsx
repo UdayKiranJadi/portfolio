4ba4d536478b7221ded9adc33f79597bc5a99bf1
Uday Kiran Jadi <udayjadi4@gmail.com>
2026-01-17 15:52:40 -0800

    navbar

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="transition-all duration-200 h-[12vh] z-[10000] fixed w-full">
+        <div className='flex items-center h-fill'></div>
+    </div>
+    
+  )
+}
+
+export default Nav
